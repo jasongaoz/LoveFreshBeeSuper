f637501296957cec7b2333afdfe1b2dcd253cf90
Adam <Yin_Y@yinyuandeMacBook-Pro.local>
2016-11-20 21:08:24 +0800

    创建闪送超市左侧模型

---
+++ LoveFreshBeeSuper/LoveFreshBeeSuper/Classes/Order/Models/AFBOrderLeftModel.m
@@ -0,0 +1,13 @@
+//
+//  AFBOrderLeftModel.m
+//  LoveFreshBeeSuper
+//
+//  Created by Adam on 2016/11/20.
+//  Copyright © 2016年 gao2015. All rights reserved.
+//
+
+#import "AFBOrderLeftModel.h"
+
+@implementation AFBOrderLeftModel
+
+@end
