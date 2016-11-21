11674fd09ba72c2f2da2ef1d277ab91292ca33b5
Adam <Yin_Y@yinyuandeMacBook-Pro.local>
2016-11-21 20:47:40 +0800

    过滤model中的id作为可以

--- LoveFreshBeeSuper/LoveFreshBeeSuper/Classes/Order/Views/AFBOrderRightCell.m
+++ LoveFreshBeeSuper/LoveFreshBeeSuper/Classes/Order/Views/AFBOrderRightCell.m
@@ -9,7 +9,20 @@
 #import "AFBOrderRightCell.h"
 #import "AFBOrderIncreaseAndReduceView.h"
 
-@implementation AFBOrderRightCell
+@interface AFBOrderRightCell ()
+@property (weak, nonatomic) IBOutlet UIImageView *iconView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *specificsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *partnerPriceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *marketPriceLabel;
+
+
+
+@end
+
+@implementation AFBOrderRightCell{
+    UIView *_lineView;
+}
 
 
 - (void)awakeFromNib {
@@ -23,7 +36,7 @@
 - (void)setupUI{
     AFBOrderIncreaseAndReduceView *increaseAndReduceView = [[[UINib nibWithNibName:@"AFBOrderIncreaseAndReduceView" bundle:nil] instantiateWithOwner:nil options:nil] lastObject];
     
-    [self addSubview:increaseAndReduceView];
+    [self.contentView addSubview:increaseAndReduceView];
     
     //increaseAndReduceView的布局size
     CGSize size = increaseAndReduceView.bounds.size;
@@ -32,17 +45,28 @@
         make.right.bottom.equalTo(self).offset(-8);
     }];
     
+    //MARK:设置label的特殊属性
+    [self.nameLabel setFont:[UIFont fontWithName:@"Helvetica-Bold" size:14]];
+    [self.partnerPriceLabel setFont:[UIFont fontWithName:@"Helvetica-Bold" size:14]];
+    
+    NSDictionary *attribtDic = @{NSStrikethroughStyleAttributeName: [NSNumber numberWithInteger:NSUnderlineStyleSingle]};
+    
+    NSMutableAttributedString *attribtStr = [[NSMutableAttributedString alloc]initWithString:@"原价" attributes:attribtDic];
+    
+    self.marketPriceLabel.attributedText = attribtStr;
 }
 
 //让label在被选中状态背景颜色不变
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
     self.contentView.backgroundColor = [UIColor whiteColor];
+     [self.selectedBackgroundView addSubview:_lineView];
 }
 
 //让label在高亮状态背景颜色不变
 - (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated{
     [super setHighlighted:highlighted animated:YES];
     self.contentView.backgroundColor = [UIColor whiteColor];
+     [self.selectedBackgroundView addSubview:_lineView];
 }
 @end
